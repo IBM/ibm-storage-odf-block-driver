6b9e14a1578395b0f068edd442563d427d98156e
Bob Lau <46341043+lbscorpio@users.noreply.github.com>
2021-07-08 21:18:20 +0800

    action: split different actions (#24)
    * action: split different actions
    
    Signed-off-by: Bo Liu <lbseraph@gmail.com>
    
    * Skip golint check
    
    Not check the the insecure
    
    * Fix: fix typo
    
    Remove the space before nosec
    
    Co-authored-by: xuezhiy <xuezhiy@cn.ibm.com>

--- pkg/rest/client.go
+++ pkg/rest/client.go
@@ -70,6 +70,7 @@ func NewRequester(p Poster) *Requester {
 
 func NewFSRestClient(config *Config) (*FSRestClient, error) {
 	tr := &http.Transport{
+		// #nosec
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		Dial: (&net.Dialer{
 			Timeout: 5 * time.Second,
